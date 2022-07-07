29f7a06474b4a5f2c6d9023f7d75a74274f44b7b
ThamaraRD <rdthata@gmail.com>
2022-07-07 17:47:15 -0400

    primer ejercicio java

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+
+    public static void suma ( int num1, int num2, int num3 ) {
+        int resultado = num1 + num2 + num3;
+       System.out.println(resultado);
+    }
+
+    public static class Coche {
+        static int numPuertas = 4;
+        void addPuertas ( int numPuertas ){
+            System.out.println(Coche.numPuertas);
+        }
+    }
+
+
+    public static void main(String[] args) {
+        suma(10,20,30);
+       Coche miCoche = new Coche();
+        miCoche.addPuertas(1);
+    }
+}
